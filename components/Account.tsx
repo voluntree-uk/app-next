c44442b93ee80c948a4f2bf5075d3d0cd25db62f
Harry Davies <harryndavies@pm.me>
2022-05-27 00:16:36 +0100

    first commit

---
+++ components/Account.tsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
+import Avatar from "./Avatar";
+
+export default function Account({ session }: any) {
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState<null | string>(null);
+  const [avatar_url, setAvatarUrl] = useState<null | string>(null);
+
+  useEffect(() => {
+    getProfile();
+  }, [session]);
+
+  async function getProfile() {
+    try {
+      setLoading(true);
+      const user = supabase.auth.user();
+
+      if (!user) {
+        return;
+      }
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username, website, avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateProfile({ username, website, avatar_url }: any) {
+    try {
+      setLoading(true);
+      const user = supabase.auth.user();
+
+      if (!user) {
+        return;
+      }
+
+      const updates = {
+        id: user.id,
+        username,
+        website,
+        avatar_url,
+        updated_at: new Date(),
+      };
+
+      let { error } = await supabase.from("profiles").upsert(updates, {
+        returning: "minimal", // Don't return the value after inserting
+      });
+
+      if (error) {
+        throw error;
+      }
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="form-widget">
+      <Avatar
+        url={avatar_url}
+        size={150}
+        onUpload={(url: any) => {
+          setAvatarUrl(url);
+          updateProfile({ username, avatar_url: url });
+        }}
+      />
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" value={session.user.email} disabled />
+      </div>
+      <div>
+        <label htmlFor="username">Name</label>
+        <input
+          id="username"
+          type="text"
+          value={username || ""}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          className="button block primary"
+          onClick={() => updateProfile({ username, avatar_url })}
+          disabled={loading}
+        >
+          {loading ? "Loading ..." : "Update"}
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="button block"
+          onClick={() => supabase.auth.signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
+}
