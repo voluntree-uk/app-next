f0f6d8e186cb56b35b7f2ebb39a482562b902b6b
Harry Davies <harryndavies@pm.me>
2022-05-27 17:53:53 +0100

    Moved from magiclink

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,77 +1,199 @@
-import { Box, Flex, Heading, Text, useToast } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+  Button,
+  Box,
+  Text,
+  Flex,
+  Heading,
+  Center,
+  Switch,
+} from "@chakra-ui/react";
+import React from "react";
 import { useForm } from "react-hook-form";
-import { supabase } from "../utils/supabaseClient";
+import { useToast } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { BsArrowRightCircleFill } from "react-icons/bs";
 import FormInput from "./FormInput";
-import { FiSend } from "react-icons/fi";
-import ActionButton from "./ActionButton";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { supabase } from "../utils/supabaseClient";
 
-type AuthFormState = {
-  email: string;
-};
+enum Mode {
+  LOGIN,
+  SIGNUP,
+  RESETPASSWORD,
+}
 
-export default function Auth() {
-  const [loading, setLoading] = useState(false);
+/**
+ * Renders form for logging in or signing up a user
+ */
+export default function AuthenticationForm(): JSX.Element {
+  const [mode, setMode] = React.useState<Mode>(Mode.LOGIN);
+
+  const isLogginIn = () => mode === Mode.LOGIN;
+  const isResettingPassword = () => mode === Mode.RESETPASSWORD;
+  const isSigningUp = () => mode === Mode.SIGNUP;
 
   const toast = useToast();
 
-  const {
-    register,
-    reset,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<AuthFormState>();
-
-  const handleLogin = async (formData: AuthFormState) => {
-    try {
-      setLoading(true);
-      const { error } = await supabase.auth.signIn({ email: formData.email });
-      if (error) throw error;
-
-      toast({
-        title: "Check your email!",
-        status: "info",
-        duration: 3000,
-        isClosable: true,
-      });
-      reset();
-    } catch (error: any) {
-      alert(error.error_description || error.message);
-    } finally {
-      setLoading(false);
-    }
+  const toggleMode = () => {
+    reset();
+    setMode((prevMode) => {
+      if (prevMode === Mode.LOGIN) {
+        return Mode.SIGNUP;
+      }
+      return Mode.LOGIN;
+    });
   };
 
-  return (
-    <Box h="100vh" bgImage="linear-gradient(to top, #dfe9f3 0%, white 100%);">
-      <Box pt={44} m={"0 auto"} w={{ base: "100%", md: "40%", lg: "40%" }}>
-        <Heading size={"xl"} mb={7} color="gray.700">
-          Login
-        </Heading>
-        <Text mb={7} color="gray.700">
-          Sign in via magic link with your email below
-        </Text>
+  const router = useRouter();
 
-        <form onSubmit={handleSubmit(handleLogin)}>
-          <Flex alignItems={"baseline"}>
-            <FormInput
-              field="email"
-              placeholder="youremail@email.com"
-              register={register}
-              type="email"
-              isVisible
-              error={errors["email"]}
-            />
-            <ActionButton
-              text="Send"
-              icon={<FiSend />}
-              type="submit"
-              loading={loading}
-              style={{ marginLeft: 5, width: "30%" }}
-            />
-          </Flex>
-        </form>
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<any>();
+
+  async function onSubmit(formData: any): Promise<void> {
+    switch (mode) {
+      case Mode.SIGNUP:
+        await supabase.auth.signUp({
+          email: formData.email,
+          password: formData.password,
+        });
+        break;
+      case Mode.LOGIN:
+        const signInReponse = await supabase.auth.signIn({
+          email: formData.email,
+          password: formData.password,
+        });
+
+        if (signInReponse.error === null) {
+          router.push("/");
+        }
+        break;
+      case Mode.RESETPASSWORD:
+        // code block
+        break;
+      default:
+      // code block
+    }
+  }
+
+  const inputsToDisplay: any[] = [
+    {
+      field: "fullName",
+      type: "text",
+      label: "Full name",
+      isVisible: isSigningUp(),
+    },
+    { field: "email", type: "email", label: "abc@email.com", isVisible: true },
+    {
+      field: "password",
+      type: "password",
+      label: "Your password",
+      // hide password if resetting email
+      isVisible: !isResettingPassword(),
+    },
+  ];
+
+  return (
+    <Box
+      mt={40}
+      pl={9}
+      pr={9}
+      pb={9}
+      w={{ base: "100%", md: "40%", lg: "40%" }}
+    >
+      <Box mb={5}>
+        <Heading fontSize={"2xl"} fontWeight="bold">
+          {isLogginIn() && "Sign in"}
+          {isSigningUp() && "Sign up"}
+          {isResettingPassword() && "Reset password"}
+        </Heading>
+
+        {isResettingPassword() && (
+          <Text fontSize={"sm"} mt={3} w={"95%"}>
+            Please enter your email address to request a password reset
+          </Text>
+        )}
       </Box>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid templateColumns="repeat(1, 1fr)" gap={4}>
+          {inputsToDisplay.map((input, index) => (
+            <GridItem key={index}>
+              <FormInput
+                field={input.field}
+                type={input.type}
+                placeholder={input.label}
+                isVisible={input.isVisible}
+                error={errors[input.field]}
+                register={register}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+
+        <Flex
+          justifyContent={"space-between"}
+          alignItems="center"
+          display={isResettingPassword() ? "none" : "flex"}
+          mt={5}
+        >
+          {!isSigningUp() && (
+            <Text fontSize={"sm"} display="flex" alignItems={"center"}>
+              <Switch size={"md"} mr={2} colorScheme="brand.700" isChecked />
+              Remember Me
+            </Text>
+          )}
+
+          {!isSigningUp() && (
+            <Text
+              fontSize={"sm"}
+              style={{ cursor: "pointer" }}
+              onClick={() => setMode(Mode.RESETPASSWORD)}
+            >
+              Forgot Password?
+            </Text>
+          )}
+        </Flex>
+
+        <Center mt={10} mb={6}>
+          <Button
+            color="white"
+            size="md"
+            type="submit"
+            isLoading={isSubmitting}
+            w="75%"
+            boxShadow={"2xl"}
+            rightIcon={<BsArrowRightCircleFill />}
+            py={7}
+            borderRadius="2xl"
+            bg="brand.700"
+          >
+            {isLogginIn() && "SIGN IN"}
+            {isSigningUp() && "SIGN UP"}
+            {isResettingPassword() && "SEND"}
+          </Button>
+        </Center>
+
+        <Center mb={6}>
+          <Text color="gray.400" fontSize={"md"} fontWeight="bold">
+            OR
+          </Text>
+        </Center>
+
+        <Center>
+          <Text fontSize={"sm"}>
+            {isLogginIn()
+              ? "Don't have an account?"
+              : "Already have an account?"}{" "}
+            <strong onClick={toggleMode} style={{ cursor: "pointer" }}>
+              {isLogginIn() ? "Sign up" : "Sign in"}
+            </strong>
+          </Text>
+        </Center>
+      </form>
     </Box>
   );
 }
