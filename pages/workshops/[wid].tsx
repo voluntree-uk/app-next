510337f5ee1da13bf4f2c885ac98e412aa5116d3
Harry Davies <harryndavies@pm.me>
2022-05-27 16:54:30 +0100

    Workshop WID working

--- pages/workshops/[wid].tsx
+++ pages/workshops/[wid].tsx
@@ -1,5 +1,38 @@
+import { Box } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React from "react";
+import Layout from "../../components/Layout";
+import { supabase } from "../../utils/supabaseClient";
 
 export default function Workshop() {
-  return <div>[wid]</div>;
+  const router = useRouter();
+
+  const { wid } = router.query;
+
+  const [workshop, setWorkshop] = React.useState<any>();
+
+  const getWorkshop = React.useCallback(async () => {
+    if (wid) {
+      let { data, error } = await supabase
+        .from("workshops")
+        .select("*")
+        .eq("id", wid);
+
+      if (data) {
+        setWorkshop(data[0]);
+      }
+    }
+  }, [wid]);
+
+  React.useEffect(() => {
+    getWorkshop();
+  }, [getWorkshop]);
+
+  return (
+    <Layout>
+      <Box>{workshop.name}</Box>
+      <Box>{workshop.description}</Box>
+      <Box>{workshop.category}</Box>
+    </Layout>
+  );
 }
