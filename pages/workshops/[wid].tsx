b04c37fbf740cab188409620118b816634cc69f0
Harry Davies <harryndavies@pm.me>
2022-05-29 14:54:35 +0100

    Started Workshop UI

--- pages/workshops/[wid].tsx
+++ pages/workshops/[wid].tsx
@@ -1,36 +1,45 @@
-import { Box } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import React from "react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Img,
+  Skeleton,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout";
+import { Workshop } from "../../shared/schemas";
 import { supabase } from "../../utils/supabaseClient";
 
-export default function Workshop() {
-  const router = useRouter();
-
-  const { wid } = router.query;
-
-  const [workshop, setWorkshop] = React.useState<any>();
-
-  const getWorkshop = React.useCallback(async () => {
-    if (wid) {
-      let { data, error } = await supabase
-        .from("workshops")
-        .select("*")
-        .eq("id", wid);
-
-      if (data) {
-        setWorkshop(data[0]);
-      }
-    }
-  }, [wid]);
-
-  React.useEffect(() => {
-    getWorkshop();
-  }, [getWorkshop]);
-
+export default function WorkshopListing({ data }: { data: Workshop[] }) {
+  const [loaded, setLoaded] = useState(false);
+  4;
+  const workshop = data[0];
   return (
     <Layout>
-      <Box></Box>
+      <Box>
+        <Skeleton isLoaded={loaded}>
+          <Img
+            alt=""
+            onLoad={() => setLoaded(true)}
+            src="https://www.namecoinnews.com/wp-content/uploads/2021/03/Basic-Forex-Trading-Styles.jpg"
+          />
+        </Skeleton>
+        <Box p={4} borderTopWidth={1} borderTopColor="gray.300">
+          <Heading size={"lg"} mb={3}>
+            {workshop.name}
+          </Heading>
+          <Text>{workshop.description}</Text>
+        </Box>
+      </Box>
     </Layout>
   );
+}
+export async function getServerSideProps(context: any) {
+  const id = context.query.wid;
+
+  let { data } = await supabase.from("workshops").select("*").eq("id", id);
+
+  return { props: { data } };
 }
