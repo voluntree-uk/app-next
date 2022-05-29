d7adc8c7ad5f3611dde6acefe3e8912578bed05a
Harry Davies <harryndavies@pm.me>
2022-05-29 14:00:27 +0100

    WorkshopCard image loading

--- components/WorkshopCard.tsx
+++ components/WorkshopCard.tsx
@@ -1,18 +1,33 @@
 import { ViewIcon } from "@chakra-ui/icons";
-import { Box, Button, Heading, Img, Tag, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  Img,
+  Skeleton,
+  Tag,
+  Text,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { Workshop } from "../shared/schemas";
 
 export default function WorkshopCard({ workshop }: { workshop: Workshop }) {
   const router = useRouter();
+  const [loaded, setLoaded] = useState(false);
 
   return (
     <Box
       cursor={"pointer"}
       onClick={() => router.push(`/workshops/${workshop.id}`)}
     >
-      <Img src="https://www.namecoinnews.com/wp-content/uploads/2021/03/Basic-Forex-Trading-Styles.jpg" />
+      <Skeleton isLoaded={!loaded}>
+        <Img
+          alt=""
+          onLoad={() => setLoaded(true)}
+          src="https://www.namecoinnews.com/wp-content/uploads/2021/03/Basic-Forex-Trading-Styles.jpg"
+        />
+      </Skeleton>
       <Box borderTopWidth={1} borderTopColor={"gray.100"} p={4} bg="white">
         <Box mb={4}>
           <Heading color={"gray.700"} size={"sm"} fontWeight="semibold" py={2}>
