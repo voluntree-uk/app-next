df2626bc966cbcdef3357b79b47cd6d548cf4367
Harry Davies <harryndavies@pm.me>
2022-05-30 22:33:30 +0100

    Temporary loading spinner on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Spinner } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/workshops");
+  }, [router]);
+
   return (
-    <Layout>
-      <Box>Welcome</Box>
-    </Layout>
+    <Box
+      h="100vh"
+      bg="brand.800"
+      display="flex"
+      alignItems={"center"}
+      justifyContent="center"
+    >
+      <Spinner
+        thickness="10px"
+        speed="0.65s"
+        color="green.300"
+        size="xl"
+        transform={"scale(2)"}
+        mb={40}
+      />
+    </Box>
   );
 }
