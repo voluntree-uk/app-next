b8139126821360d46d4abb64862c6bd3325a34ef
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2022-10-03 19:36:40 +0100

    Document workshopToReadableAddress

--- utils/addresses.ts
+++ utils/addresses.ts
@@ -1,5 +1,10 @@
 import { Workshop } from "../shared/schemas";
 
-export function workshopToReadableAddress(workshop: Workshop) {
+/**
+ * Returns a readable address given the workshop
+ * @param workshop a workshop for which the address is to be built
+ * @returns a built readable address
+ */
+export function workshopToReadableAddress(workshop: Workshop): string {
   return `${workshop.house} ${workshop.street}, ${workshop.postcode}`;
 }
