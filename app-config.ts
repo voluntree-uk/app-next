17840ec43d6b73a60e2f9e82baab12bf6f937006
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2022-12-17 13:11:08 +0000

    Add FilterProps and TimeFilter definitions to the schema

--- app-config.ts
+++ app-config.ts
@@ -1,5 +1,8 @@
+import { TimeFilter } from "./shared/schemas";
+
 const config = {
   categories: ["technology", "finance", "languages", "arts", "legal", "other"],
+  timeFilters: Object.values(TimeFilter)
 };
 
 export default config;
