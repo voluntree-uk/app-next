bbb45a30feed2635f2aff86a0486ad058e55cd91
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2024-05-18 12:14:57 +0100

    Redirect to user profile page from /me

--- pages/me/index.tsx
+++ pages/me/index.tsx
@@ -1,26 +1,21 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-import { User } from "@supabase/supabase-js";
 import { auth } from "@auth/supabase";
-import Account from "@components/Account";
-import Layout from "@components/Layout/Layout";
 
-export default function MePage({ user }: { user: User }) {
-  return (
-    <Layout>
-      <Box>{user ? <Account user={user} /> : null}</Box>
-    </Layout>
-  );
+export default function MePage() {
+  return;
 }
 
 export async function getServerSideProps({ req }: any) {
   const user = await auth.getUserByCookie(req);
 
   if (!user) {
-    return { props: {}, redirect: { destination: "/" } };
+    return {
+      props: {},
+      redirect: { destination: "/" }
+    };
+  } else {
+    return {
+      props: { user },
+      redirect: { destination: `/user/${user.id}` },
+    };
   }
-
-  return {
-    props: { user },
-  };
 }
