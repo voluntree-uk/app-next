dc60cbf0da6a8f5b36230a8cfda19beb95e2035b
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2024-05-18 17:56:47 +0100

    Clean up workshop endpoint server side props

--- pages/workshops/[wid].tsx
+++ pages/workshops/[wid].tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { Booking, Slot, Workshop } from "@schemas";
-import { User } from "@supabase/supabase-js";
 import { data } from "@data/supabase";
-import { auth } from "@auth/supabase";
 import Layout from "@components/Layout/Layout";
 import WorkshopListing from "@components/Workshop/WorkshopListing";
 
 export default function Wid({
   workshop,
   slots,
-  bookings,
-  user,
+  bookings
 }: {
   workshop: Workshop;
   slots: Slot[];
   bookings: Booking[];
-  user: User;
 }) {
   return (
     <Layout>
@@ -27,14 +23,12 @@ export default function Wid({
 export async function getServerSideProps(context: any) {
   const id = context.query.wid;
 
-  const user = await auth.getUserByCookie(context.req);
-
   try {
     const workshop = await data.getWorkshop(id);
     const slots = await data.getWorkshopSlots(id);
     const bookings = await data.getWorkshopBookings(id);
 
-    return { props: { workshop, slots, bookings, user } };
+    return { props: { workshop, slots, bookings } };
   } catch (error) {
     console.log(JSON.stringify(error));
 
