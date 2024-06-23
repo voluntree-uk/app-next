9de14bec691eebce75609498e2553385e778d967
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2024-06-23 18:54:08 +0100

    Add dynamic loading to workshop list and item pages

--- app/workshops/[wid]/page.tsx
+++ app/workshops/[wid]/page.tsx
@@ -1,28 +1,15 @@
 "use server"
 
 import WorkshopListing from "@components/Workshop/WorkshopListing";
-import { SupabaseDataAccessor } from "@data/supabase";
 import { createClient } from "@util/supabase/server";
 
-export default async function Page({ params }: { params: any }) {
+export default async function Page({ params }: { params: { wid: string } }) {
   const supabase = createClient();
-  const data = new SupabaseDataAccessor(supabase);
-
-  const id = params.wid;
-
-  const workshop = await data.getWorkshop(id);
-  const host = await data.getProfile(workshop.user_id);
-  const slots = await data.getWorkshopSlots(id);
-  const bookings = await data.getWorkshopBookings(id);
-
   const { data: { user } } = await supabase.auth.getUser();
 
   return (
     <WorkshopListing
-      workshop={workshop}
-      host={host}
-      slots={slots}
-      bookings={bookings}
+      workshop_id={params.wid}
       user={user}
     />
   );
