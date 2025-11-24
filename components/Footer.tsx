1b0e1f82fe02e04acc2cef7234357c8ee0bab967
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2025-11-24 21:16:24 +0000

    Update second footer reference to contact page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -102,7 +102,7 @@ export default function Footer({
             <Link
               as={NextLink}
               target="_blank"
-              href={"https://voluntree.net/get-involved/"}
+              href={"/contact"}
             >
               Get involved
             </Link>
